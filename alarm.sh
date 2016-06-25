501f72ec9ccc5b33bfe7d42a71cb10366d14142d
Seenkoo <shinkupro@gmail.com>
2016-06-25 18:36:39 +0500

    Now we can add more than 1 alarm

--- alarm.sh
+++ alarm.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# usage: sh ./alarm.sh <hh:mm> <alarm_file>
+# usage: alarm <hh:mm> <alarm_file>
 
 ALARM_TIME="$1"
 ALARM_FILE="$2"
 PID=$$
-PID_FILE="/usr/local/var/run/alarm.pid"
-# Save PID file
+PID_FILE="/usr/local/var/run/alarm_${ALARM_TIME}.pid"
 echo $PID > "$PID_FILE"
 
 # Stop playing and exit when notification is dismissed or script is stopped
@@ -28,7 +27,7 @@ caffeinate -u -d -w $PID &
 # Box notification
 osascript -e "with timeout 601 seconds" \
           -e "tell application \"SystemUIServer\" to activate" \
-          -e "display dialog \"It's $ALARM_TIME now!\" buttons \"Stop\" default button 1 with title \"Alarm\" giving up after 600" \
+          -e "display dialog \"It's $ALARM_TIME now!\" buttons \"Stop\" default button 1 with title \"Alarm\" giving up after 300" \
           -e "do shell script \"kill $PID\"" \
           -e "end timeout" &
 
